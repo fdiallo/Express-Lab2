b88c451d58676caf6f14efce9cff9820725ce845
fdiallo <fallaye.diallo@gmail.com>
2026-05-14 15:53:16 -0400

    Task 5: Handle and Respond with Data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // Dependencies
 const express = require("express")
 const app = express()
+const axios = require("axios")
 require("dotenv").config()
 const PORT = process.env.PORT
 
@@ -12,8 +13,22 @@ const PORT = process.env.PORT
 
 
 // Routes
-app.get("/api/fun-fact", (req, res) => {
-    res.send("This route will be responsible for fetching the data and sending it to the client.")
+app.get("/api/fun-fact", async (req, res) => {
+    try {
+        //res.send("This route will be responsible for fetching the data and sending it to the client.")
+        const response = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+
+        res.json(response.data)
+        console.log(response.data)
+    } catch (error) {
+        if (error.response) {
+            console.error('API Error:', error.response.status, error.response.data);
+            res.status(error.response.status).json({ message: 'Error fetching data from external API.' });
+        } else {
+            console.error('Network Error:', error.message);
+            res.status(500).json({ message: 'A network error occurred.' });
+        }
+    }
 })
 
 
