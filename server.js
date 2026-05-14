31af0f5788b39125e57c50f12d04d1374592c050
fdiallo <fallaye.diallo@gmail.com>
2026-05-14 13:25:36 -0400

    Task 2: Create the Express Server

---
+++ server.js
@@ -0,0 +1,23 @@
+
+// Dependencies
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const PORT = process.env.PORT
+
+
+// Middlewares
+
+
+
+
+// Routes
+
+
+
+
+
+// Port
+app.listen(PORT, () => {
+    console.log(`Server is listening to PORT: ${PORT}`)
+})
